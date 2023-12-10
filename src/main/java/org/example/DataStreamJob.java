4ec2e744aefff6fec21b3082d356fedce016e121
Tyler Delamater <tdelamater@gmail.com>
2023-12-09 20:13:08 -0600

    initial commit

---
+++ src/main/java/org/example/DataStreamJob.java
@@ -0,0 +1,70 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.example;
+
+import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+
+import java.sql.Timestamp;
+import java.time.Duration;
+
+public class DataStreamJob {
+
+    public static void main(String[] args) throws Exception {
+        // set up the streaming execution environment
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        System.out.println("test");
+        //env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+
+        DataStreamSource<String> data = env.socketTextStream("localhost", 9090);
+
+
+        data.map(new MapFunction<String, Tuple2<Long, String>>() {
+                    public Tuple2<Long, String> map(String s) {
+                        String[] words = s.split(",");
+                        return new Tuple2<Long, String>(Long.parseLong(words[0]), words[1]);
+                    }
+                }).assignTimestampsAndWatermarks(WatermarkStrategy.<Tuple2<Long, String>>forBoundedOutOfOrderness(Duration.ofSeconds(5))
+                        .withTimestampAssigner(new SerializableTimestampAssigner<Tuple2<Long, String>>() {
+                            @Override
+                            public long extractTimestamp(Tuple2<Long, String> t, long l) {
+                                return t.f0;
+                            }
+                        }))
+                .windowAll(TumblingEventTimeWindows.of(Time.seconds(5)))
+                .reduce(new ReduceFunction<Tuple2<Long, String>>() {
+                    public Tuple2<Long, String> reduce(Tuple2<Long, String> t1, Tuple2<Long, String> t2) {
+                        int num1 = Integer.parseInt(t1.f1);
+                        int num2 = Integer.parseInt(t2.f1);
+                        int sum = num1 + num2;
+                        Timestamp t = new Timestamp(System.currentTimeMillis());
+                        return new Tuple2<Long, String>(t.getTime(), "" + sum);
+                    }
+                }).print();
+        // execute program
+        env.execute("Window");
+    }
+}
